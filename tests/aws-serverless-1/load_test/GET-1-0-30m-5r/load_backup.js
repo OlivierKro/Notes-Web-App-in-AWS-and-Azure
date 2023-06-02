e017434ce05b131116932084824696209ed0f97b
OlivierKro <olivier.kroll99@gmail.com>
2023-06-02 23:02:23 +0200

    Add tests files from AWS serverless architecture 1

---
+++ tests/aws-serverless-1/load_test/GET-1-0-30m-5r/load_backup.js
@@ -0,0 +1,28 @@
+import http from 'k6/http';
+import { check, sleep, data } from 'k6';
+
+export const options = {
+	discardResponseBodies: true,
+	scenarios: {
+	  contacts: {
+		executor: 'constant-arrival-rate',
+		duration: '30m',
+		timeUnit: '1s',
+		rate: 5,
+		preAllocatedVUs: 5,
+		maxVUs: 20
+	  },
+	},
+};
+
+export default function () {
+	let headers = {
+		'app_user_name': 'Test_user',
+		'Content-Type': 'application/json'
+	};
+	const res = http.get('https://2pkvb43g77.execute-api.eu-central-1.amazonaws.com/prod/notes', { headers: headers });
+	check(res, { 'status was 200': (r) => r.status == 200 });
+	sleep(1);
+}
+
+
---
+++ tests/aws-serverless-1/load_test/POST-1-0-30m-5r/load_backup.js
@@ -0,0 +1,33 @@
+import http from 'k6/http';
+import { check, sleep, data } from 'k6';
+
+export const options = {
+	discardResponseBodies: true,
+	scenarios: {
+	  contacts: {
+		executor: 'constant-arrival-rate',
+		duration: '30m',
+		timeUnit: '1s',
+		rate: 5,
+		preAllocatedVUs: 5,
+		maxVUs: 20
+	  },
+	},
+};
+
+export default function () {
+	let headers = {
+		'app_user_name': 'Test_user',
+		'Content-Type': 'application/json'
+	};
+	let body = {
+		"Item": {
+			"title": "Test title",
+			"category": "Stress tests",
+			"description": "Stress test on cloud",
+		}
+	};
+	const res = http.post('https://2pkvb43g77.execute-api.eu-central-1.amazonaws.com/prod/note', JSON.stringify(body), { headers: headers });
+	check(res, { 'status was 200': (r) => r.status == 200 });
+	sleep(1);
+}
