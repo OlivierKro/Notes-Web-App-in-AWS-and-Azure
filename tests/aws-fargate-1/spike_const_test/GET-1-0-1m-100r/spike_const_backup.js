73a7c05ef3241779e7957c602ce15e4fda0ce64d
OlivierKro <106828122+OlivierKro@users.noreply.github.com>
2023-07-06 22:02:35 +0200

    Tests files in containers

---
+++ tests/aws-fargate-1/spike_const_test/GET-1-0-1m-100r/spike_const_backup.js
@@ -0,0 +1,70 @@
+import http from 'k6/http';
+import { check, sleep, data } from 'k6';
+
+export const options = {
+	discardResponseBodies: true,
+	scenarios: {
+	  contacts: {
+		exec: 'getNotes',
+		executor: 'ramping-arrival-rate',
+		startRate: 0,
+		timeUnit: '1s',
+		preAllocatedVUs: 0,
+		maxVUs: 1000,
+		stages: [
+			{ target: 100, duration: '25s' },
+			{ target: 100, duration: '10s' },
+			{ target: 0, duration: '25s' },
+			{ target: 0, duration: '4m' },
+
+			{ target: 100, duration: '25s' },
+			{ target: 100, duration: '10s' },
+			{ target: 0, duration: '25s' },
+			{ target: 0, duration: '4m' },
+
+			{ target: 100, duration: '25s' },
+			{ target: 100, duration: '10s' },
+			{ target: 0, duration: '25s' },
+			{ target: 0, duration: '4m' },
+
+			{ target: 100, duration: '25s' },
+			{ target: 100, duration: '10s' },
+			{ target: 0, duration: '25s' },
+			{ target: 0, duration: '4m' },
+
+			{ target: 100, duration: '25s' },
+			{ target: 100, duration: '10s' },
+			{ target: 0, duration: '25s' },
+		]
+	  },
+	},
+};
+
+export function getNotes () {
+	let headers = {
+		'app_user_name': 'Test_user',
+		'Content-Type': 'application/json'
+	};
+	const res = http.get('http://3.68.221.34/notes', { headers: headers });
+//	const res = http.get('https://notes-webapp.lemonflower-c4cb3782.germanywestcentral.azurecontainerapps.io/notes', { headers: headers });
+	check(res, { 'status was 200': (r) => r.status == 200 });
+	sleep(1);
+}
+
+export function addNote () {
+	let headers = {
+		'app_user_name': 'Test_user',
+		'Content-Type': 'application/json'
+	};
+	let body = {
+		"Item": {
+			"title": "Test title",
+			"category": "Stress tests",
+			"description": "Stress test on cloud",
+		}
+	};
+//	const res = http.post('http://3.68.221.34/note', JSON.stringify(body), { headers: headers });
+	const res = http.post('https://notes-webapp.lemonflower-c4cb3782.germanywestcentral.azurecontainerapps.io/note', JSON.stringify(body), { headers: headers });
+ 	check(res, { 'status was 200': (r) => r.status == 200 });
+	sleep(1);
+}
